2ae771ee0ec75881d79e7e8ac5579f36bd8aae56
MattiaSarti <nmattia1@gmail.com>
2025-07-01 16:32:10 +0200

    feat: add work-in-progress actual poetry migration

--- scripts/kf1288-poetry-migration/main.py
+++ scripts/kf1288-poetry-migration/main.py
@@ -24,74 +24,12 @@ PATH_FOR_REPOSITORY_LIST = Path("../../presets/kubeflow-repos.yaml")
 logger = setup_logging(log_level="INFO", logger_name=__name__)
 
 
-def update_tox_installation_and_checkout_actions(content: str) -> str:
+def migrate_to_poetry(directory: str) -> bool:
     return (
-        content
-        .replace("actions/checkout@v2", "actions/checkout@v4")
-        .replace("actions/checkout@v3", "actions/checkout@v4")
-        .replace("pip install tox", "pipx install tox")
+        update_tox_ini(_dir=directory)
+        and update_pyproject_toml(_dir=directory)
+        and update_lock_file_and_exported_charm_requirements(_dir=directory)
     )
-
-
-def migrate_to_poetry(repo: Client, charms: list[LocalCharmRepo], dry_run: bool) -> None:
-    logger.info(f"processing repo at '{repo.base_path}'...")
-
-    logger.info("\tadding contributing instructions...")
-    contributing_file_path_from_script = PATH_FOR_THIS_SCRIPT_SUBFOLDER / "CONTRIBUTING.md"
-    contributing_file_path_in_repo = repo.base_path / "CONTRIBUTING.md"
-    if not exists(contributing_file_path_in_repo):
-        copy(contributing_file_path_from_script, contributing_file_path_in_repo)
-    else:
-        with open(contributing_file_path_from_script, "r") as source_file:
-            with open(contributing_file_path_in_repo, "wa") as preexisting_target_file:
-                preexisting_target_file.write("\n\n")
-                preexisting_target_file.write(source_file.read())
-    if repo.is_dirty():
-        repo.update_branch(
-            commit_msg="docs: add instructions for dependency management",
-            directory=".",
-            push=not dry_run,
-            force=True
-        )
-
-    logger.info("\tmodifying workflows using tox...")
-    for ci_file_path in (repo.base_path / ".github" / "workflows").glob("*.yaml"):
-        with open(ci_file_path, "r") as file:
-            file_content = file.read()
-        updated_file_content = update_tox_installation_and_checkout_actions(content=file_content)
-        with open(ci_file_path, "w") as file:
-            file.write(updated_file_content)
-    if repo.is_dirty():
-        repo.update_branch(
-            commit_msg="ci: let tox find poetry as an external dependency",
-            directory=".",
-            push=not dry_run,
-            force=True
-        )
-
-    raise NotImplementedError
-    # for charm in charms:
-    #     charm_folder = (repo.base_path / charm.tf_module).parent
-    #     copy(CURRENT_FOLDER / "charmcraft.yaml", charm_folder / "charmcraft.yaml" )
-    #     if repo.is_dirty():
-    #         repo.update_branch(
-    #             commit_msg=f"build: ...TODO... for charm {charm.name}",
-    #             directory=".",
-    #             push=not dry_run,
-    #             force=True
-    #         )
-
-    #     success = update_deps(CURRENT_FOLDER / "update-deps.sh", charm_folder )
-    #     if not success:
-    #         logging.warning(f"Failing to update dependencies on charm {charm.name}")
-    #     if success and repo.is_dirty():
-    #         repo.update_branch(
-    #             commit_msg=f"updating deps for charm {charm.name}",
-    #             directory=".",
-    #             push=not dry_run, force=True
-    #         )
-
-    # success = update_deps(CURRENT_FOLDER / "update-deps.sh", repo.base_path)
 
 
 def main() -> None:
@@ -107,11 +45,81 @@ def main() -> None:
     )
 
     client.canon_run(
-        wrapper_func=migrate_to_poetry,
+        wrapper_func=process_repository,
         branch_name="just-a-dry-run",  # TODO
         title="build: migrate to poetry for Python dependency management",
         body="",  # TODO
         dry_run=True
+    )
+
+
+def process_repository(repo: Client, charms: list[LocalCharmRepo], dry_run: bool) -> None:
+    logger.info(f"processing repo at '{repo.base_path}'...")
+
+    commit_message = "docs: add instructions for dependency management"
+    logger.info(f"\timplementing commit '{commit_message}'")
+    contributing_file_path_from_script = PATH_FOR_THIS_SCRIPT_SUBFOLDER / "CONTRIBUTING.md"
+    contributing_file_path_in_repo = repo.base_path / "CONTRIBUTING.md"
+    if not exists(contributing_file_path_in_repo):
+        copy(contributing_file_path_from_script, contributing_file_path_in_repo)
+    else:
+        with open(contributing_file_path_from_script, "r") as source_file:
+            with open(contributing_file_path_in_repo, "wa") as preexisting_target_file:
+                preexisting_target_file.write("\n\n")
+                preexisting_target_file.write(source_file.read())
+    if repo.is_dirty():
+        repo.update_branch(commit_msg=commit_message, directory=".", push=not dry_run, force=True)
+
+    commit_message = "ci: update tox installation and checkout actions"
+    logger.info(f"\timplementing commit '{commit_message}'")
+    for ci_file_path in (repo.base_path / ".github" / "workflows").glob("*.yaml"):
+        with open(ci_file_path, "r") as file:
+            file_content = file.read()
+        updated_file_content = update_tox_installation_and_checkout_actions(content=file_content)
+        with open(ci_file_path, "w") as file:
+            file.write(updated_file_content)
+    if repo.is_dirty():
+        repo.update_branch(commit_msg=commit_message, directory=".", push=not dry_run, force=True)
+
+    commit_message_beginning = "build: migrate to poetry for dependency management"
+    logger.info(f"\timplementing alls commits related to '{commit_message}'")
+    for charm in charms:
+        actual_commit_message = f"{commit_message_beginning} in charm '{charm.name}'"
+        logger.info(f"\timplementing commit '{actual_commit_message}'")
+        charm_folder = (repo.base_path / charm.tf_module).parent
+        success = migrate_to_poetry(directory=charm_folder)
+        if success and repo.is_dirty():
+            repo.update_branch(commit_msg=actual_commit_message, directory=".", push=not dry_run, force=True)
+        elif not success:
+            logger.error(f"\t\tfailed implementing commit '{actual_commit_message}'")
+    actual_commit_message = f"{commit_message_beginning} in base project folder"
+    logger.info(f"\t\timplementing commit '{actual_commit_message}'")
+    base_project_folder = (repo.base_path / charm.tf_module).parent
+    success = migrate_to_poetry(directory=base_project_folder)
+    if success and repo.is_dirty():
+        repo.update_branch(commit_msg=actual_commit_message, directory=".", push=not dry_run, force=True)
+    elif not success:
+        logger.error(f"\t\tfailed implementing commit '{actual_commit_message}'")
+
+
+def update_lock_file_and_exported_charm_requirements(_dir: str) -> bool:
+    raise NotImplementedError
+
+
+def update_pyproject_toml(_dir: str) -> bool:
+    raise NotImplementedError
+
+
+def update_tox_ini(_dir: str) -> bool:
+    raise NotImplementedError
+
+
+def update_tox_installation_and_checkout_actions(content: str) -> str:
+    return (
+        content
+        .replace("actions/checkout@v2", "actions/checkout@v4")
+        .replace("actions/checkout@v3", "actions/checkout@v4")
+        .replace("pip install tox", "pipx install tox")
     )
 
 
